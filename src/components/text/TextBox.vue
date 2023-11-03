a37af15770e3d749f0e597eba04fe4e89307011f
Jonathan Braun <yonibrown@gmail.com>
2023-11-03 09:32:51 +0200

    scrollable1

--- src/components/text/TextBox.vue
+++ src/components/text/TextBox.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <h3>Text</h3> -->
-  <div class="win_text bible-text in_body" ref="textBox">
+  <div class="win_text bible-text in_body scrollable">
     <text-verse
       v-for="vrs in verses"
       :key="vrs.part_id"
@@ -11,7 +11,7 @@
 
 <script setup>
 import TextVerse from "./TextVerse.vue";
-import { inject, ref, watch, provide } from "vue";
+import { inject, ref, watch } from "vue";
 import { sendToServer } from "../../server.js";
 
 const elementId = inject("elementId");
@@ -19,8 +19,6 @@ const hasToReload = inject("hasToReload");
 const reloaded = inject("reloaded");
 
 const verses = ref([]);
-const textBox = ref(null);
-provide("textBox", textBox);
 
 watch(hasToReload, function (newVal) {
   if (newVal) {
--- src/components/text/TextWord.vue
+++ src/components/text/TextWord.vue
@@ -9,7 +9,6 @@ import { computed, inject, ref, onMounted } from "vue";
 const props = defineProps(["word", "verse"]);
 const linkIds = inject("linkIds");
 const getCategory = inject("getCategory");
-const textBox = inject("textBox");
 
 const wordObj = ref(null);
 
@@ -28,43 +27,15 @@ const categoryStyle = computed(() => {
   return {}; // no category linked
 });
 onMounted(function () {
-  if (props.word.word == "נְדִיבִים") {
-    console.log(props.word);
-    console.log(
-      'textBox.scrollTop',
-      textBox.value.scrollTop,
-    );
-    console.log(
-      'textBox.scrollHeight',
-      textBox.value.scrollHeight,
-    );
-    console.log(
-      'textBox.offsetTop',
-      textBox.value.offsetTop,
-    );
-    console.log(
-      'wordObj.offsetTop',
-      wordObj.value.offsetTop,
-    );
-  }
   if (props.word.anchor) {
-    // console.log()
-    // wordObj.value.scrollIntoView({behavior: "smooth", block: 'nearest', inline: 'nearest' });
-    // console.log(textBox.value.scrollTop, wordObj.value.offsetTop);
-    textBox.value.scrollTop = wordObj.value.offsetTop - textBox.value.offsetTop;
-    // console.log(textBox, wordObj);
-    // console.log(
-    //   textBox.value.scrollTop,
-    //   textBox.value.scrollHeight,
-    //   wordObj.value.offsetTop
-    // );
-    // wordObj.value.scrollTo({
-    //   top: textBox.value.offsetTop,
-    //   left: 0,
-    //   behavior: "smooth",
-    // });
+    scrollIn(wordObj.value);
   }
 });
+
+function scrollIn(elm) {
+  const scrollable = elm.closest(".scrollable");
+  scrollable.scrollTop = elm.offsetTop - scrollable.offsetTop;
+}
 </script>
 
 <style scoped></style>
