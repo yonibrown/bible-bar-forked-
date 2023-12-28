bc183d423418b8f28a9b5ff29ce9d18bce7251ee
Jonathan Braun <yonibrown@gmail.com>
2023-12-29 01:29:10 +0200

    calss2

--- src/components/project/links.js
+++ src/components/project/links.js
@@ -4,7 +4,6 @@ import { biLink } from "./biLink.js";
 
 // class
 export function useLinks({ storeMethods, projId }) {
-
   // access objects
   function getLink(prop) {
     return biLink.getLink(prop);
@@ -31,56 +30,24 @@ export function useLinks({ storeMethods, projId }) {
   // }
 
   // access database
-  async function setName(prop, newName) {
+  function setName(prop, newName) {
     biLink.setName(prop, newName);
   }
 
-  async function updateCategory(link, cat, attr) {
+  function updateCategory(link, cat, attr) {
     link.updateCategory(cat, attr);
   }
 
-  async function addElementToLink(link, elmId) {
-    if (!link.elements.includes(elmId)) {
-      link.elements.push(elmId);
-    }
-
-    const data = {
-      type: "link",
-      oper: "add_elm",
-      id: linkObjId(link),
-      prop: { elm: elmId },
-    };
-    const obj = await storeMethods.prj.sendToServer(data);
-    const elm = storeMethods.elm.getElement(elmId);
-    storeMethods.elm.reload(elm, { add_link: true });
+  function addElementToLink(link, elmId) {
+    link.addElementToLink(elmId);
   }
 
-  async function removeElementFromLink(link, elmId) {
+  function removeElementFromLink(link, elmId) {
     link.removeElementFromLink(elmId);
   }
 
-  async function createLink(options) {
-    const prop = {
-      proj: projId,
-      research_id: options.researchId,
-      main_element: options.element.id,
-    };
-
-    const data = {
-      type: "link",
-      oper: "new",
-      id: { dummy: "" },
-      prop,
-    };
-    const obj = await sendToServer(data);
-
-    links.value.push(obj.data);
-    // options.element.verses = [];
-    storeMethods.elm.reload(options.element);
-  }
-
-  function reload(link) {
-    link.reload();
+  function createLink(options) {
+    biLink.createLink(options);
   }
 
   function reloadResLink(resIdObj) {
