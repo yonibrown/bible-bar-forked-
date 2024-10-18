c4d0c454ea6d1d4c9905e74ad5a6d0c17a5e5629
Jonathan Braun <yonibrown@gmail.com>
2024-10-18 14:55:04 +0300

    verse range5

--- src/components/ui/SpecTable/internal/ColumnTitle.vue
+++ src/components/ui/SpecTable/internal/ColumnTitle.vue
@@ -1,7 +1,23 @@
 <template>
-  <span>{{ fld.title }}</span>
+  <base-editable
+    :initialValue="fld.title"
+    @submitValue="submitTitle"
+    name="fieldTitle"
+    :getDefault="defaultName"
+    :disabled="!enableSelection"
+    placeholder="הוסף כותרת..."
+    :blankable="true"
+  ></base-editable>
 </template>
 
 <script setup>
+import { inject } from "vue";
 const props = defineProps(["fld"]);
+const enableSelection = inject("enableSelection");
+
+function submitTitle(title) {}
+
+function defaultName() {
+  console.log("defaultName");
+}
 </script>
